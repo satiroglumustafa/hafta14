2760d292b9fc4665175116200d15c47e447acd4c
satiroglumustafa <mmustafa.satiroglu@gmail.com>
2024-09-08 19:31:55 +0300

    Eleman Dışında alanın tıklandığını göstern hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
+import { useRef } from 'react';
 import './App.css';
+import { useOnClickOutside } from './hooks/useOnClickOutside';
 
 function App() {
+
+  const ref1 = useRef()
+
+  function disarisiTiklandi(){
+    alert("Elemanın Dışında bir Alan tıklandı")
+  }
+
+  useOnClickOutside(ref1,disarisiTiklandi) // bu fonksiyondan geriye herhangibir değer gelmediği için doğrudan çalışırtık
+
   return (
      <>
-      App 1
+        <div ref={ref1} id='kutu1'>
+            Kutu 1
+        </div>
+
      </>
   );
 }
 
 export default App;
+ 
